0826ff0ff2b452197dabd8ba47813812ece2a7ac
Pavlo Patoka <patokapn@gmail.com>
2021-02-20 23:22:57 +0000

    V 0.999

--- terraform/init-script.sh
+++ terraform/init-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common python3-pip -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update -y

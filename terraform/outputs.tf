c7ba588dc3fc52d241e97c7beea1fc9bf005866a
Pavlo Patoka <patokapn@gmail.com>
2021-02-20 22:50:34 +0000

    V 0.996

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_endpoint" {
   description = "The address of the EC2 instance"
-  value       = aws_instance.linux-instance.public_ip
+  value       = aws_instance.linux-srv.public_ip
 }
